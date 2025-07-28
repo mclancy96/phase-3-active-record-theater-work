f3f0d737e5e44b5b7bfb8a7799d795d35a0f54a2
Mike <mclancy96@gmail.com>
2025-07-28 14:21:27 -0400

    Role methods

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,21 @@
 class Role < ActiveRecord::Base
   has_many :auditions
+
+  def actors
+    auditions.map(&:actor)
+  end
+
+  def locations
+    auditions.map(&:location)
+  end
+
+  def lead
+    lead_audition = auditions.find { |audition| audition.hired == true }
+    lead_audition || 'no actor has been hired for this role'
+  end
+
+  def understudy
+    understudy_audition = auditions.select { |audition| audition.hired == true }
+    (understudy_audition && understudy_audition[1]) || 'no actor has been hired for understudy for this role'
+  end
 end
